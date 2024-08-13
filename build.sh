0b679b86cc1bc5d354719ef2aa7219c642b7da34
Chris Johnson <chrej791@gmail.com>
2024-08-13 15:04:45 -0400

    Tweak Dockerfile and build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-git pull
 HASH=$(git rev-parse --short HEAD)
 docker build -t chrej/plantaid-server:latest -t chrej/plantaid-server:$HASH .
-docker push -a chrej/plantaid-server
+docker push chrej/plantaid-server:$HASH
+docker push chrej/plantaid-server:latest
 echo "Pushed image tag: $HASH"
